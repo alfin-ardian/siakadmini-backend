ccdd5ea2dc167701a19b432bc340c1a5099819cd
alfin ardian <developer.alfin@gmail.com>
2020-12-15 15:24:32 +0700

    update model mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -4,4 +4,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model{
     protected $tabel = 'siakad_mini.mahasiswa';
+    protected $guarded = [];
+
+
+    public function jurusan(){
+        return $this->belongsTo('App\Jurusan','id_jurusan','id');
+    }
 }
